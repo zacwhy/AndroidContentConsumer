e9747ce2c5981cab2181c853408c5af8cb592b09
zy <zacwhy@gmail.com>
2012-11-01 15:44:28 +0800

    modified to handle if no menu

--- src/com/example/contentconsumer/MenuActivity.java
+++ src/com/example/contentconsumer/MenuActivity.java
@@ -37,48 +37,62 @@ public class MenuActivity extends ListActivity {
 		Intent intent = getIntent();
 		long menuId = intent.getLongExtra(EXTRA_MENU_ID, 0);
 		
-		ContentMenuManager menuManager = ContentMenuFactory.getMenuManager(this);
-		currentMenu = menuManager.findMenuById(menuId);
+		currentMenu = getCurrentMenu(menuId, this);
 		
-		if (currentMenu.isRoot()) {
-			ContentMenu rootMenu = currentMenu;
-			
-			ContentMenu firstChild = rootMenu.getFirstChild();
-			
-			if (firstChild == null) {
-				currentMenu = new ContentMenu(0, "No menus", null);
-			} else {
-				menuId = firstChild.getId();
-				currentMenu = menuManager.findMenuById(menuId);
-			}
+		if (currentMenu != null) {
+			setTitle(currentMenu.getTitle());
+			populateList();
 		}
-		
-		String title = currentMenu.getTitle();
-		setTitle(title);
-		
-		String[] menuTextArray = ContentMenuHelper.getMenuTextArray(currentMenu.getChildren());
-		setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_menu, menuTextArray));
 
 		ListView listView = getListView();
 		listView.setTextFilterEnabled(true);
 
 		listView.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				Context applicationContext = getApplicationContext();
-				
-				List<ContentMenu> childMenus = currentMenu.getChildren();
-				ContentMenu nextMenu = childMenus.get(position);
-				
-				Class<?> cls = nextMenu.hasChildren() ? MenuActivity.class : ContentActivity.class;
-				
-				Intent intent = new Intent(applicationContext, cls);
-				intent.putExtra(EXTRA_MENU_ID, nextMenu.getId());
-				startActivity(intent);
+				onItemClick2(parent, view, position, id);
 				
 				//String text = "No children";
 				//Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show();
 			}
 		});
+	}
+	
+	private static ContentMenu getCurrentMenu(long menuId, Context context) {
+		ContentMenuManager menuManager = ContentMenuFactory.getMenuManager(context);
+		ContentMenu menu = menuManager.findMenuById(menuId);
+		
+		Boolean showRootMenu = false;
+		if (!showRootMenu && menu.isRoot()) {
+			ContentMenu rootMenu = menu;
+			ContentMenu firstChild = rootMenu.getFirstChild();
+			
+			if (firstChild == null) {
+				return null;
+			}
+			
+			menuId = firstChild.getId();
+			menu = menuManager.findMenuById(menuId);
+		}
+		
+		return menu;
+	}
+
+	private void populateList() {
+		String[] menuTextArray = ContentMenuHelper.getMenuTextArray(currentMenu.getChildren());
+		setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_menu, menuTextArray));
+	}
+	
+	private void onItemClick2(AdapterView<?> parent, View view, int position, long id) {
+		Context applicationContext = getApplicationContext();
+		
+		List<ContentMenu> childMenus = currentMenu.getChildren();
+		ContentMenu nextMenu = childMenus.get(position);
+		
+		Class<?> cls = nextMenu.hasChildren() ? MenuActivity.class : ContentActivity.class;
+		
+		Intent intent = new Intent(applicationContext, cls);
+		intent.putExtra(EXTRA_MENU_ID, nextMenu.getId());
+		startActivity(intent);
 	}
 
 	@Override
--- src/com/example/contentconsumer/support/ContentMenuFactory.java
+++ src/com/example/contentconsumer/support/ContentMenuFactory.java
@@ -1,16 +1,10 @@
 package com.example.contentconsumer.support;
 
-//import java.util.List;
-
 import android.content.Context;
-
-//import com.example.contentconsumer.database.DbMenu;
-//import com.example.contentconsumer.database.DbMenuDataSource;
 
 public class ContentMenuFactory {
 	
 	private static ContentMenuManager menuManager;
-	//private static ContentMenu menus;
 	
 	public static ContentMenuManager getMenuManager(Context context) {
 		// todo dictionary
@@ -19,91 +13,5 @@ public class ContentMenuFactory {
 		}
 		return menuManager;
 	}
-	
-//	public static ContentMenu getMenus(Context context) {
-//		if (menus == null) {
-//			menus = buildMenus(context);
-//		}
-//		return menus;
-//	}
-//	
-//	public static ContentMenu getMenus() {
-//		if (menus == null) {
-//			menus = buildMenus();
-//		}
-//		return menus;
-//	}
-//	
-//	public static ContentMenu findMenuById(long id) {
-//		return ContentMenuFactory.getMenus().findDescendantById(id);
-//	}
-//	
-//	private static ContentMenu buildMenus(Context context) {
-//		ContentMenu rootMenu = ContentMenu.newRootMenu();
-//		
-//		List<DbMenu> dbMenus = getAllMenus(context);
-//		
-//		return rootMenu;
-//	}
-//	
-//	private static List<DbMenu> getAllMenus(Context context) {
-//		DbMenuDataSource dataSource = new DbMenuDataSource(context);
-//		dataSource.open();
-//		
-//		initDb(dataSource); // todo remove
-//		
-//		List<DbMenu> menus = dataSource.getAllMenus();
-//		dataSource.close();
-//		return menus;
-//	}
-//	
-//	// todo remove
-//	private static void initDb(DbMenuDataSource dataSource) {
-//		DbMenu menu1 = dataSource.insertMenu("Menu 1", 0, 1);
-//		DbMenu menu2 = dataSource.insertMenu("Menu 2", 0, 2);
-//		DbMenu menu3 = dataSource.insertMenu("Menu 3", 0, 3);
-//		
-//		DbMenu menu1_1 = dataSource.insertMenu("Menu 1-1", menu1.getId(), 1);
-//		DbMenu menu1_2 = dataSource.insertMenu("Menu 1-2", menu1.getId(), 1);
-//		DbMenu menu1_3 = dataSource.insertMenu("Menu 1-3", menu1.getId(), 1);
-//		
-//		DbMenu menu2_1 = dataSource.insertMenu("Menu 2-1", menu2.getId(), 1);
-//		DbMenu menu2_2 = dataSource.insertMenu("Menu 2-2", menu2.getId(), 1);
-//		
-//		DbMenu menu3_1 = dataSource.insertMenu("Menu 3-1", menu3.getId(), 1);
-//		
-//		DbMenu menu1_1_1 = dataSource.insertMenu("Menu 1-1-1", menu1_1.getId(), 1);
-//	}
-//	
-//	private static ContentMenu buildMenus() {
-//		ContentMenu rootMenu = ContentMenu.newRootMenu();
-//
-//		// todo get data from SQLite
-//		ContentMenu menu1 = createNewMenu(1, "Menu 1", rootMenu);
-//
-//		ContentMenu menu1_1 = createNewMenu(2, "Menu 1-1", menu1);
-//		createNewMenu(3, "Menu 1-2", menu1);
-//		createNewMenu(4, "Menu 1-3", menu1);
-//
-//		createNewMenu(5, "Menu 1-1-1", menu1_1);
-//
-//		ContentMenu menu2 = createNewMenu(6, "Menu 2", rootMenu);
-//
-//		createNewMenu(7, "Menu 2-1", menu2);
-//
-//		ContentMenu menu3 = createNewMenu(8, "Menu 3", rootMenu);
-//
-//		createNewMenu(9, "Menu 3-1", menu3);
-//		
-//		createNewMenu(10, "The quick brown fox jumps over the lazy dog.", rootMenu);
-//
-//		return rootMenu;
-//	}
-//	
-//	private static ContentMenu createNewMenu(long id, String title, ContentMenu parent) {
-//		ContentMenu menu = new ContentMenu(id, title, parent);
-//		parent.getChildren().add(menu);
-//		return menu;
-//	}
 	
 }
